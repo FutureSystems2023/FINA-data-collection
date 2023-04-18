6c6301485f56a76fb1f0ef5d1496bb5d2103fc77
FSOnline23 <futuresystems@outlook.sg>
2023-04-18 19:03:06 +0800

    Updates 18042023

--- script.py
+++ script.py
@@ -18,35 +18,31 @@ apiEndPoint = "https://api.worldaquatics.com/fina/rankings/swimming/report/csv"
 # Parameters for requests
 distance = "200"  # e.g. (50, 100, 200, 400, 800, 1500) Change as appropriate
 gender = "F"  # e.g. (M or F) Change as appropriate
-stroke = "FREESTYLE_RELAY"  # e.g. (FREESTYLE, FREESTYLE_RELAY) Change as appropriate
+stroke = "FREESTYLE"  # e.g. (FREESTYLE, FREESTYLE_RELAY) Change as appropriate
 poolConfiguration = "LCM"
 year = ""  # year leave blank if filtering by date
 startDate = "01%2F01%2F2019"
-endDate = "12%2F31%2F2022"
+endDate = "05%2F05%2F2023"
 timesMode = "ALL_TIMES"
 pageSize = "200"
 countryId = ""
 
 # Define Countries to get results from
 countries_list = [
-    "Philippines",
     "Singapore",
+    "Philippines",
     "Malaysia",
     "Vietnam",
     "Indonesia",
     "Thailand",
-    "Brunei Darussalam",
-    "Myanmar",
-    "Lao People's Democratic Republic",
-    "Cambodia",
-    "Democratic Republic of Timor - Leste",
+    "Myanmar"
 ]
 
 # Declaration of Variables for looping
 countryId_list = []
 csv = []
 gender_full = ""
-
+excelFileName = " ".join([gender, distance, stroke]) + ".xlsx"
 
 # Read Countries.json File to obtain country ID
 def getCountryID():
@@ -95,12 +91,13 @@ def callAPI():
 
 # Compile CSV into one excel file
 def compileCSV():
-    excelFileName = " ".join([gender, distance, stroke]) + ".xlsx"
     writer = pd.ExcelWriter(excelFileName)
+    df = pd.DataFrame()
     for i in range(len(csv)):
-        df = pd.read_csv(csv[i])
-        sheet_name = csv[i]
-        df.to_excel(writer, sheet_name=sheet_name[0:-4], index=False)
+        df_csv = pd.read_csv(csv[i])
+        df = pd.concat([df, df_csv], axis=0)
+    df.drop(df[df['meet_name'] == "meet_name"].index, inplace=True)
+    df.to_excel(writer,sheet_name="RAW", index=False)
     writer.save()
 
 
@@ -110,7 +107,21 @@ def deleteCSVs():
         os.remove(csv[i])
 
 
+# Filter RAW Data by Athlete Name defined in namelist.csv
+def filterNames():
+    writer = pd.ExcelWriter(excelFileName)
+    df = pd.read_excel(excelFileName)
+    df_namelist = pd.read_csv('namelist.csv',header=None)
+    df_filtered = pd.DataFrame()
+    for i in range(df_namelist.shape[0]):
+        for j in range(df_namelist.shape[1]):
+            df_filtered = pd.concat([df_filtered, df[df['full_name_computed'] == df_namelist.at[i, j]]])
+    df.to_excel(writer,sheet_name="Competitors 2019-2023", index=False)
+    writer.save()
+
+
 def main():
+    '''
     print(" ".join(["Getting results for:", gender, distance, stroke]))
     print("Countries: " + ", ".join(countries_list))
     getCountryID()
@@ -118,6 +129,7 @@ def main():
     compileCSV()
     deleteCSVs()
     print("Results downloaded and compiled succesfully!")
-
+    '''
+    filterNames()
 
 main()
