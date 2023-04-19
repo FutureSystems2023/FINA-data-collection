dd6a853119c4248261d1be59bfda88e82f4973e5
King Yeung Lee <kingyeung129@hotmail.com>
2023-04-19 13:04:10 +0800

    Initial Commit

--- script.py
+++ script.py
@@ -16,9 +16,9 @@ import os
 apiEndPoint = "https://api.worldaquatics.com/fina/rankings/swimming/report/csv"
 
 # Parameters for requests
-distance = "200"  # e.g. (50, 100, 200, 400, 800, 1500) Change as appropriate
+distance = "400"  # e.g. (50, 100, 200, 400, 800, 1500) Change as appropriate
 gender = "F"  # e.g. (M or F) Change as appropriate
-stroke = "FREESTYLE_RELAY"  # e.g. (FREESTYLE, FREESTYLE_RELAY) Change as appropriate
+stroke = "MEDLEY"  # e.g. (FREESTYLE, FREESTYLE_RELAY) Change as appropriate
 poolConfiguration = "LCM"
 year = ""  # year leave blank if filtering by date
 startDate = "01%2F01%2F2019"
